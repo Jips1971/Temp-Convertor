55a79a002f49c965baece18da82d9efde906a2b3
James Blonden <james@blonden.co.uk>
2025-06-19 11:19:32 +0100

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Temp Convert VVB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("AM1S230")>
+<Assembly: AssemblyProduct("Temp Convert VVB")>
+<Assembly: AssemblyCopyright("Copyright © AM1S230 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e434088f-402f-40ef-8cb8-6b77fd0a4fbc")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
